f001d38cbbc0c045002814fffe245aae96a65036
Ken Takaki <kenkiki94305@gmail.com>
2017-12-21 22:52:12 +0900

    added sort according to part value

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import os
+from collections import Counter
 
 def main(filename):
     f = open(filename)
@@ -16,6 +17,7 @@ def main(filename):
     with open(csv_name, "w", newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
         writer.writeheader()
+        smd_parts = []
         for line in lines:
             line = line.replace("\n", "")    
             eagle_part = (list(filter(None, line.split(" "))))
@@ -28,6 +30,14 @@ def main(filename):
             smd_part["Layer"] = "T"
             smd_part["Rotation"] = eagle_part[3]
             smd_part["Comment"] = eagle_part[4]
+            smd_parts.append(smd_part)
+        comment_counts = Counter([smd_part["Comment"] for smd_part in smd_parts]).most_common()
+        comment_counts = {element[0]: element[1] for element in comment_counts}
+        smd_parts.sort(key=lambda x: x["Comment"])        
+        smd_parts.sort(key=lambda x: comment_counts[x["Comment"]], reverse=True)
+
+
+        for smd_part in smd_parts:
             writer.writerow(smd_part)
         
         
